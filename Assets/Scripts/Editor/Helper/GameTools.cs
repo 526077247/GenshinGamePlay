3e60248fc6b14b368b818a601d2de7c37070055c
Tailor <526077247@qq.com>
2023-02-06 09:51:02 +0800

    Shift+B 快速打开启动场景

---
+++ Assets/Scripts/Editor/Helper/GameTools.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace TaoTie
+{
+    public class GameTools
+    {
+        [MenuItem("Tools/帮助/启动场景 #_b")]
+        static void ChangeInitScene()
+        {
+            EditorSceneManager.OpenScene("Assets/AssetsPackage/Scenes/InitScene/Init.unity");
+        }
+    }
+}
