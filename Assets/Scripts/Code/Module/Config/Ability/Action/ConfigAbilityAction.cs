a4b860682a7e53dcc4b552f48ce0d3fbab16a80b
Tailor <526077247@qq.com>
2023-02-08 19:02:29 +0800

    Update ConfigAbilityAction.cs

--- Assets/Scripts/Code/Module/Config/Ability/Action/ConfigAbilityAction.cs
+++ Assets/Scripts/Code/Module/Config/Ability/Action/ConfigAbilityAction.cs
@@ -4,24 +4,32 @@
     {
         public AbilityTargetting Targetting;
         public ConfigSelectTargets OtherTargets;
+        public ConfigAbilityPredicate Predicate;
+        public ConfigAbilityPredicate PredicateForeach; 
         protected abstract void Execute(Entity applier, ActorAbility ability, ActorModifier modifier, Entity target);
 
         public void DoExecute(Entity applier, ActorAbility ability, ActorModifier modifier, Entity target)
         {
-            Entity[] targetLs = AbilityHelper.ResolveTarget(applier, ability, modifier, target,Targetting, OtherTargets);
-            if (targetLs != null && targetLs.Length > 0)
+            if (Predicate == null || Predicate.Evaluate(applier,ability,modifier,target))
             {
-                foreach (Entity item in targetLs)
+                Entity[] targetLs = AbilityHelper.ResolveTarget(applier, ability, modifier, target,Targetting, OtherTargets);
+                if (targetLs != null && targetLs.Length > 0)
                 {
-                    if (item != null)
+                    foreach (Entity item in targetLs)
                     {
-                        Execute(applier, ability, modifier, item);
+                        if (item != null)
+                        {
+                            if (PredicateForeach == null || PredicateForeach.Evaluate(applier, ability, modifier, item))
+                            {
+                                Execute(applier, ability, modifier, item);
+                            }
+                        }
                     }
                 }
-            }
-            else
-            {
-                Log.Error("[ConfigAbilityAction:DoExecute] resolve action target is illegal,please check logic!");
+                else
+                {
+                    Log.Error("[ConfigAbilityAction:DoExecute] resolve action target is illegal,please check logic!");
+                }
             }
         }
     }
