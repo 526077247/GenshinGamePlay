80ede4359734ac8e4415b638282165c8d7802472
Tailor <526077247@qq.com>
2023-02-08 19:32:54 +0800

    监听状态机状态

---
+++ Assets/Scripts/Code/Game/Component/Ability/Mixin/AttachToStateIDMixin.cs
@@ -0,0 +1,85 @@
+﻿namespace TaoTie
+{
+    public class AttachToStateIDMixin : AbilityMixin
+    {
+        public ConfigAttachToStateIDMixin config => baseConfig as ConfigAttachToStateIDMixin;
+
+        private Fsm fsm;
+        private bool hasAddModifier;
+        private Entity onwer;
+        private AbilityComponent abilityComponent;
+        public override void Init(ActorAbility actorAbility, ActorModifier actorModifier, ConfigAbilityMixin config)
+        {
+            base.Init(actorAbility, actorModifier, config);
+            onwer = actorAbility.Parent.GetParent<Entity>();
+            fsm = onwer?.GetComponent<FsmComponent>()?.GetFsm(this.config.ChargeLayer);
+            abilityComponent = onwer?.GetComponent<AbilityComponent>();
+            if (fsm != null)
+            {
+                fsm.onStateChanged += OnStateChanged;
+                if (this.config.StateIDs.Contains(fsm.currentStateName))
+                {
+                    ApplyModifier();
+                }
+            }
+        }
+
+        private void OnStateChanged(string from, string to)
+        {
+            if (config.StateIDs == null || abilityComponent == null)
+                return;
+
+            bool flag = config.StateIDs.Contains(from), flag2 = config.StateIDs.Contains(to);
+            if (!flag && flag2)
+            {
+                ApplyModifier();
+            }
+            else if (flag && !flag2)
+            {
+                RemoveModifier();
+            }
+        }
+
+        private void ApplyModifier()
+        {
+            if (EvaluatePredicate())
+            {
+                abilityComponent.ApplyModifier(onwer.Id, actorAbility, config.ModifierName);
+                hasAddModifier = true;
+            }
+        }
+
+        private bool EvaluatePredicate()
+        {
+            if (config.Predicate != null)
+            {
+                return config.Predicate.Evaluate(onwer, actorAbility, actorModifier, onwer);
+            }
+            return true;
+        }
+
+        private void RemoveModifier()
+        {
+            if (hasAddModifier)
+            {
+                abilityComponent.RemoveModifier(actorAbility.Config.AbilityName, config.ModifierName);
+            }
+
+            hasAddModifier = false;
+        }
+
+        public override void Dispose()
+        {
+            if (fsm != null)
+            {
+                fsm.onStateChanged -= OnStateChanged;
+                fsm = null;
+            }
+
+            hasAddModifier = false;
+            abilityComponent = null;
+            onwer = null;
+            base.Dispose();
+        }
+    }
+}
---
+++ Assets/Scripts/Code/Module/Config/Ability/Mixin/ConfigAttachToStateIDMixin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace TaoTie
+{
+    /// <summary>
+    /// 监听状态机状态
+    /// </summary>
+    public class ConfigAttachToStateIDMixin: ConfigAbilityMixin
+    {
+        public string ChargeLayer;
+        public HashSet<string> StateIDs;
+        public ConfigAbilityPredicate Predicate;
+        public string ModifierName;
+        public override AbilityMixin CreateAbilityMixin(ActorAbility actorAbility, ActorModifier actorModifier)
+        {
+            var res = ObjectPool.Instance.Fetch(TypeInfo<AttachToStateIDMixin>.Type) as AttachToStateIDMixin;
+            res.Init(actorAbility, actorModifier, this);
+            return res;
+        }
+    }
+}
