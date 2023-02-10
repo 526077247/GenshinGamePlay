21e0bfaf4c08e037b89d6a48839cdfc8fadd0b0e
Tailor <526077247@qq.com>
2023-02-10 18:28:18 +0800

    更新模组管理工具

--- Assets/Scripts/Code/Editor/ModuleEditor/ModuleEditor.cs
+++ Assets/Scripts/Code/Editor/ModuleEditor/ModuleEditor.cs
@@ -7,20 +7,20 @@ using UnityEngine;
 namespace TaoTIe
 {
 
-    public class ModuleEditor: EditorWindow
+    public class ModuleEditor : EditorWindow
     {
         private const string settingAsset = "Assets/Scripts/Code/Editor/ModuleEditor/ModuleInfo.asset";
         private string Source = "Modules";
         private string ModulePath = "Assets/Modules";
         private ModuleInfo info;
-        
+
         Vector2 scrollPos;
         private Dictionary<string, bool> temp = new Dictionary<string, bool>();
 
         [MenuItem("Tools/模组管理工具")]
         public static void ShowWindow()
         {
-            GetWindow(typeof (ModuleEditor));
+            GetWindow(typeof(ModuleEditor));
         }
 
         private void OnEnable()
@@ -36,6 +36,7 @@ namespace TaoTIe
                 Source = info.Source;
                 ModulePath = info.ModulePath;
             }
+
             temp.Clear();
             DirectoryInfo[] content = Array.Empty<DirectoryInfo>();
             if (Directory.Exists(ModulePath))
@@ -51,8 +52,8 @@ namespace TaoTIe
 
         private void OnGUI()
         {
-            Source = EditorGUILayout.TextField("仓库路径",Source);
-            ModulePath = EditorGUILayout.TextField("导入路径",ModulePath);
+            Source = EditorGUILayout.TextField("仓库路径", Source);
+            ModulePath = EditorGUILayout.TextField("导入路径", ModulePath);
             GUILayout.Label("");
             if (!Directory.Exists(Source))
             {
@@ -62,30 +63,45 @@ namespace TaoTIe
 
             DirectoryInfo dir = new DirectoryInfo(Source);
             var sources = dir.GetDirectories();
-            if (sources.Length==0)
+            if (sources.Length == 0)
             {
                 GUILayout.Label("暂无模组");
                 return;
             }
-            
-            scrollPos = EditorGUILayout.BeginScrollView(scrollPos,GUILayout.Width(position.width),GUILayout.Height(position.height-115));
+
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Width(position.width),
+                GUILayout.Height(position.height - 115));
             for (int i = 0; i < sources.Length; i++)
             {
                 EditorGUILayout.BeginHorizontal();
                 bool old = false;
                 temp.TryGetValue(sources[i].Name, out old);
                 temp[sources[i].Name] = GUILayout.Toggle(old, sources[i].Name);
+                if (GUILayout.Button("强制更新"))
+                {
+                    if (Directory.Exists(ModulePath + "/" + sources[i].Name))
+                    {
+                        Directory.Delete(ModulePath + "/" + sources[i].Name, true);
+                    }
+
+                    SafeCopyDir(Source + "/" + sources[i].Name, ModulePath + "/" + sources[i].Name);
+                    Debug.Log("强制更新 " + ModulePath + "/" + sources[i].Name);
+                }
+
                 if (GUILayout.Button("反向覆盖"))
                 {
                     if (Directory.Exists(Source + "/" + sources[i].Name))
                     {
-                        Directory.Delete(Source + "/" + sources[i].Name,true);
+                        Directory.Delete(Source + "/" + sources[i].Name, true);
                     }
-                    SafeCopyDir(ModulePath + "/" + sources[i].Name,  Source+ "/" + sources[i].Name);
-                    Debug.Log("反向覆盖 "+ModulePath);
+
+                    SafeCopyDir(ModulePath + "/" + sources[i].Name, Source + "/" + sources[i].Name);
+                    Debug.Log("反向覆盖 " + Source + "/" + sources[i].Name);
                 }
+
                 EditorGUILayout.EndHorizontal();
             }
+
             EditorGUILayout.EndScrollView();
             GUILayout.Space(25);
 
@@ -97,15 +113,17 @@ namespace TaoTIe
                     if (item.Value && !has)
                     {
                         SafeCopyDir(Source + "/" + item.Key, ModulePath + "/" + item.Key);
-                        Debug.Log("添加 "+item.Key);
+                        Debug.Log("添加 " + item.Key);
+                        AssetDatabase.Refresh();
                     }
 
                     if (!item.Value && has)
                     {
                         AssetDatabase.DeleteAsset(ModulePath + "/" + item.Key);
-                        Debug.Log("移除 "+item.Key);
+                        Debug.Log("移除 " + item.Key);
                     }
                 }
+
                 AssetDatabase.Refresh();
                 Close();
             }
@@ -123,28 +141,32 @@ namespace TaoTIe
             EditorUtility.SetDirty(info);
             AssetDatabase.SaveAssets();
         }
-        
+
         public static bool SafeCopyDir(string sourcePath, string savePath)
         {
             if (!Directory.Exists(savePath))
             {
                 Directory.CreateDirectory(savePath);
             }
+
             #region //拷贝labs文件夹到savePath下
+
             try
             {
-                string[] labDirs = Directory.GetDirectories(sourcePath);//目录
-                string[] labFiles = Directory.GetFiles(sourcePath);//文件
+                string[] labDirs = Directory.GetDirectories(sourcePath); //目录
+                string[] labFiles = Directory.GetFiles(sourcePath); //文件
                 if (labFiles.Length > 0)
                 {
                     for (int i = 0; i < labFiles.Length; i++)
                     {
                         //if (Path.GetExtension(labFiles[i]) == ".dll")//过滤出.dll文件
                         //{
-                        File.Copy(Path.Combine(sourcePath, Path.GetFileName(labFiles[i])), Path.Combine(savePath, Path.GetFileName(labFiles[i])), true);
+                        File.Copy(Path.Combine(sourcePath, Path.GetFileName(labFiles[i])),
+                            Path.Combine(savePath, Path.GetFileName(labFiles[i])), true);
                         //}
                     }
                 }
+
                 if (labDirs.Length > 0)
                 {
                     for (int j = 0; j < labDirs.Length; j++)
@@ -152,7 +174,8 @@ namespace TaoTIe
                         Directory.GetDirectories(Path.Combine(sourcePath, Path.GetFileName(labDirs[j])));
 
                         //递归调用
-                        SafeCopyDir(Path.Combine(sourcePath, Path.GetFileName(labDirs[j])), Path.Combine(savePath, Path.GetFileName(labDirs[j])));
+                        SafeCopyDir(Path.Combine(sourcePath, Path.GetFileName(labDirs[j])),
+                            Path.Combine(savePath, Path.GetFileName(labDirs[j])));
                     }
                 }
             }
@@ -160,7 +183,9 @@ namespace TaoTIe
             {
                 return false;
             }
+
             #endregion
+
             return true;
         }
     }
