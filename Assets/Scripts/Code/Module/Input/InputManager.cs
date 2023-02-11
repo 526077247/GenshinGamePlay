ec410ed5e30da5a96f4cf90f8e53305b081d5b47
Fang <17639377466@163.com>
2023-02-11 14:46:09 +0800

    Update InputManager.cs
    新增InputGetKey方法

--- Assets/Scripts/Code/Module/Input/InputManager.cs
+++ Assets/Scripts/Code/Module/Input/InputManager.cs
@@ -1,15 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace TaoTie
 {
-    public class InputManager: IManager, IUpdateManager
+    public class InputManager : IManager, IUpdateManager
     {
         public static InputManager Instance { get; private set; }
         public bool IsPause;
+
         /// <summary>
         /// 按键绑定
         /// </summary>
         private readonly Dictionary<int, int> keySetMap = new Dictionary<int, int>();
+
+        /// <summary>
+        /// 按键状态
+        /// </summary>
+        private readonly Dictionary<int, bool> keyStatus = new Dictionary<int, bool>();
+
         #region IManager
 
         public void Init()
@@ -20,13 +29,40 @@ namespace TaoTie
         public void Destroy()
         {
             Instance = null;
+            keySetMap.Clear();
+            keyStatus.Clear();
         }
 
         #endregion
 
         public void Update()
         {
-            if(IsPause) return;
+            if (IsPause) return;
+            foreach (var keyValue in keySetMap)
+            {
+                var keyCode = (KeyCode)keyValue.Value;
+                keyStatus[keyValue.Value] = Input.GetKey(keyCode);
+            }
+        }
+
+        /// <summary>
+        ///  获取按键是否按下
+        /// </summary>
+        /// <param name="keyCode"></param>
+        /// <returns></returns>
+        public bool InputGetKey(int keyCode)
+        {
+            if (keyStatus.ContainsKey(keyCode))
+            {
+                return keyStatus[keyCode];
+            }
+
+            Log.Error($"keyCode Fail! keyCode = {keyCode}");
+            return false;
+        }
+
+        public void SetInputKeyMap(int key, int keyCode)
+        {
         }
     }
 }
