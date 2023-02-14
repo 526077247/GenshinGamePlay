f2ba4bb519a201a416aa3171a61273341b797edf
Tailor <526077247@qq.com>
2023-02-14 22:32:59 +0800

    AI配置

--- Assets/Scripts/Code/Module/AI/ConfigAIBeta.cs
+++ Assets/Scripts/Code/Module/AI/ConfigAIBeta.cs
@@ -8,15 +8,25 @@ namespace TaoTie
     [NinoSerialize]
     public partial class ConfigAIBeta
     {
-        [NinoMember(1)][BoxGroup("技能")]
+        [NinoMember(1)][LabelText("AI类型")]
+        public bool Enable;
+        [NinoMember(2)][LabelText("AI类型")]
+        public DecisionArchetype DecisionArchetype;
+
+        [NinoMember(3)][LabelText("感知")]
+        public ConfigAISensing Sensing;
+        [NinoMember(4)][LabelText("威胁")]
+        public ConfigAIThreatSetting Threat;
+        
+        [NinoMember(5)][BoxGroup("技能")]
         public int GloabCD;
-        [NinoMember(2)][BoxGroup("技能")]
+        [NinoMember(6)][BoxGroup("技能")]
         public Dictionary<int, int> SkillGroupCDConfigs;
-        [NinoMember(3)][BoxGroup("技能")]
+        [NinoMember(7)][BoxGroup("技能")]
         public ConfigAISkill[] Skills;
-        [NinoMember(4)][BoxGroup("行为")][Tooltip("优先级从上到下")]
+        [NinoMember(8)][BoxGroup("行为")][Tooltip("优先级从上到下")]
         public ConfigAITacticBaseSetting[] Tactics;
 
-        
+
     }
 }
